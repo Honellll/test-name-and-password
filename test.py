70191413378081e230269a49099d5bc0ee18f8f3
Artyom <artyom.aberyasev@gmail.com>
2024-11-27 01:05:01 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+nickname = input("your nickname: ")
+password = input("your password: ")
+print("your nickname: ", nickname)
+print("your password: ", password)
+YesOrNo = input('OK ("yes" if yes, or write "no" if no): ')
+if YesOrNo == "no":
+    input()
+if YesOrNo == "yes":
+    description = input("your description: ")
+    
+print("nickname: ", nickname)
+print("password: ", password)
+print("description: ", description)
+input()
